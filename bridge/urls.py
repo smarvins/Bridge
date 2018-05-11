2a2cae76e1044c58a51545f9829be151cb8bfb33
Shavolski <successismyname101@gmail.com>
2018-05-11 13:39:13 +0300

    added templates and necessary changes

--- bridge/urls.py
+++ bridge/urls.py
@@ -1,8 +1,9 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 from . import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^app/', include('app.urls')),
     url(r'^$', views.homepage),
 ]

b14261507470fd2b3ceb1f7ad95143b3493ef88f
Shavolski <successismyname101@gmail.com>
2018-05-11 15:14:03 +0300

    added an app class

--- app/models.py
+++ app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class app(models.Model):
+    thumb = models.ImageField(default= 'default.png', blank=True)
+    body = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+    # author = models.ForeignKey(User, default=None)

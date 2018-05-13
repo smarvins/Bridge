62a8b78d4575f471e314e12de509db539af76344
Shavolski <successismyname101@gmail.com>
2018-05-13 12:53:52 +0300

    added name tags for the urls

--- app/urls.py
+++ app/urls.py
@@ -2,9 +2,9 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.timeline),
-    url(r'^search/$', views.search),
-    url(r'^timeline/$', views.timeline),
-    url(r'^profile/$', views.profile),
+    url(r'^$', views.timeline, name='timeline'),
+    url(r'^search/$', views.search, name='search'),
+    url(r'^timeline/$', views.timeline, name='timeline'),
+    url(r'^profile/$', views.profile, name='profile'),
     # url(r'^$', views.homepage),
 ]

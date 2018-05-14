a6cb0dd23616230c89f8d61ed8aef36d9b74db7b
Shavolski <successismyname101@gmail.com>
2018-05-14 04:12:21 +0300

    added class profile for the user

--- app/models.py
+++ app/models.py
@@ -9,3 +9,15 @@ class app(models.Model):
 
     def __str__(self):
         return self.body
+
+class profile(models.Model):
+    name = models.TextField()
+    profilepic = models.ImageField(default= 'default.png', blank=True)
+    backgroundimage = models.ImageField(default= 'default.png', blank=True)
+    bio = models.TextField()
+    email = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+    # author = models.ForeignKey(User, default=None)
+
+    def __str__(self):
+        return self.name

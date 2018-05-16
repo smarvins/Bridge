96c0df4c7a4a6a1e2660eb9dc1f81a6dcea4e36d
Shavolski <successismyname101@gmail.com>
2018-05-16 11:52:08 +0300

    registered new users and clases

--- app/admin.py
+++ app/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import app
+from .models import app,profile,Comment
 # Register your models here.
 
 admin.site.register(app)
+admin.site.register(profile)
+admin.site.register(Comment)
